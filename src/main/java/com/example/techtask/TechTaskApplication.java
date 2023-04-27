a73adf548c5ac511c1a9efd367f730d5e4c9cde8
Tom.Higgins <tom.higgins@autotrader.co.uk>
2023-04-27 14:10:10 +0100

    spring initialiser

---
+++ src/main/java/com/example/techtask/TechTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.techtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/techtask/TechTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
